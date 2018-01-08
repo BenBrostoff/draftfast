0fb8d9a94ba2e22f422cd6cf3a8ee2d666d3a144
Ben Brostoff <ben.brostoff@gmail.com>
2018-01-08 07:47:35 -0500

    Narrow down legitimate args

--- pickem/pickem_command_line.py
+++ pickem/pickem_command_line.py
@@ -2,35 +2,20 @@ import argparse
 import os
 
 OPTIMIZE_COMMAND_LINE = [
-    ['-s', 'scrape from data source', 'y'],
-    ['-w', 'current week', None],
-    ['-season', 'current season', None],
-    ['-historical', 'fetch historical data', 'n'],
-    ['-mp', 'missing players to allow', 100],
-    ['-sp', 'salary threshold to ignore', 3000],
-    ['-ms', 'max salary for player on roster', 100000],
     ['-v_avg', 'projections must be within points v avg', 100000],
-    ['-i', 'iterations to run', 1],
     ['-lp', 'lowest acceptable projection', 0],
-    ['-po', 'highest acceptable ownership', 100],
-    ['-limit', 'disallow more than 1 player per team sans QB', 'n'],
     ['-home', 'only select players playing at home', None],
-    ['-duo', 'force a QB + WR/TE duo on specific team', 'n'],
-    ['-no_double_te', 'disallow two tight end lineups', 'n'],
-    ['-teams', 'player must be on specified teams', None],
+    ['-locked_teams', 'player must be on specified teams', None],
+    ['-banned_teams', 'player must not be on specified teams', None],
     ['-locked', 'player must be in final lineup', None],
     ['-banned', 'player cannot be named players', None],
-    ['-dtype', 'specify WR or TE in combo', 'wr'],
     ['-source', 'data source to use', 'nfl_rotogrinders'],
-    ['-league', 'league', 'NFL'],
     ['-pids', 'player id file (create upload file)', None],
     ['-keep_pids', 'Keep current upload file', None],
-    ['-po_location', 'projected ownership percentages file location', None],
     ['-salary_file', 'file location for salaries',
      os.getcwd() + '/data/current-salaries.csv'],
     ['-projection_file', 'file location for projections',
      os.getcwd() + '/data/current-projections.csv'],
-    ['-flex_position', 'force player to have FLEX position', None],
     ['-randomize_projections', 'use random projection factor', None],
     ['-min_avg', 'player must exceed average points', 0],
 ]
@@ -54,5 +39,6 @@ def get_args():
             opt[0],
             nargs=nargs,
             help=opt[1],
-            default=opt[2])
+            default=opt[2]
+        )
     return PARSER.parse_args()
