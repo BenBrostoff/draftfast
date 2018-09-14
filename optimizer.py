3a1865be85372b724f01f20378897a9056a36011
Ben Brostoff <ben.brostoff@gmail.com>
2018-09-14 18:03:10 -0400

    Fix bug with generator on enumerated players

--- optimizer.py
+++ optimizer.py
@@ -1,5 +1,6 @@
 from ortools.linear_solver import pywraplp
 
+
 class Optimizer(object):
     def __init__(
         self,
@@ -15,7 +16,7 @@ class Optimizer(object):
             pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING
         )
         self.players = players
-        self.enumerated_players = enumerate(players)
+        self.enumerated_players = list(enumerate(players))
         self.existing_rosters = existing_rosters
         self.salary = salary
         self.roster_size = roster_size
@@ -24,11 +25,17 @@ class Optimizer(object):
 
         self.player_to_idx_map = {}
         self.variables = []
-        for idx, player in enumerate(players):
+        for idx, player in self.enumerated_players:
             if player.lock and not player.multi_position:
-                self.variables.append(self.solver.IntVar(1, 1, player.solver_id))
+                self.variables.append(
+                    self.solver.IntVar(
+                        1, 1, player.solver_id
+                    )
+                )
             else:
-                self.variables.append(self.solver.IntVar(0, 1, player.solver_id))
+                self.variables.append(
+                    self.solver.IntVar(0, 1, player.solver_id)
+                )
 
             self.player_to_idx_map[player.solver_id] = idx
 
@@ -40,11 +47,13 @@ class Optimizer(object):
         self._set_salary_cap()
         self._set_roster_size()
         self._set_positions()
+        self._set_stack()
+        self._set_combo()
         solution = self.solver.Solve()
         return solution == self.solver.OPTIMAL
 
     def _optimize_on_projected_points(self):
-        for i, player in enumerate(self.players):
+        for i, player in self.enumerated_players:
             proj = player.proj if self.settings.projection_file \
                 else player.average_score
             self.objective.SetCoefficient(
@@ -57,7 +66,7 @@ class Optimizer(object):
             0,
             self.salary,
         )
-        for i, player in enumerate(self.players):
+        for i, player in self.enumerated_players:
             salary_cap.SetCoefficient(
                 self.variables[i],
                 player.cost
@@ -72,32 +81,42 @@ class Optimizer(object):
         for variable in self.variables:
             size_cap.SetCoefficient(variable, 1)
 
-    # def _set_stack(self):
-    #     if args.__dict__.get('stack') and args.__dict__.get('stack_count'):
-    #         position_cap = self.solver.Constraint(.stack_count, args.stack_count)
-    #
-    #         for i, player in enumerate(self.players):
-    #             if args.stack == player.team:
-    #                 position_cap.SetCoefficient(
-    #                     self.variables[i],
-    #                     1
-    #                 )
+    def _set_stack(self):
+        stack = self.settings.__dict__.get('stack')
+        stack_count = self.settings.__dict__.get('stack_count')
+
+        if stack and stack_count:
+            position_cap = self.solver.Constraint(
+                stack_count,
+                stack_count,
+            )
+
+            for i, player in self.enumerated_players:
+                if self.settings.stack == player.team:
+                    position_cap.SetCoefficient(
+                        self.variables[i],
+                        1
+                    )
 
     def _set_combo(self):
-        teams = set(p.team for p in self.players)
-        enumerated_players = enumerate(self.players)
-        for team in teams:
-            wrs_on_team = [
-                self.variables[i] for i, p in enumerated_players
-                if p.team == team and p.pos == 'WR'
-            ]
-            qbs_on_team = [
-                self.variables[i] for i, p in enumerated_players
-                if p.team == team and p.pos == 'QB'
-            ]
-            self.solver.Add(
-                self.solver.Sum(wrs_on_team) >= self.solver.Sum(qbs_on_team)
-            )
+        combo = self.settings.__dict__.get('force_combo')
+        if combo:
+            teams = set([p.team for p in self.players])
+            enumerated_players = self.enumerated_players
+
+            for team in teams:
+                wrs_on_team = [
+                    self.variables[i] for i, p in enumerated_players
+                    if p.team == team and p.pos == 'WR'
+                ]
+                qbs_on_team = [
+                    self.variables[i] for i, p in enumerated_players
+                    if p.team == team and p.pos == 'QB'
+                ]
+                self.solver.Add(
+                    self.solver.Sum(wrs_on_team) >=
+                    self.solver.Sum(qbs_on_team)
+                )
 
     def _set_positions(self):
         for position, min_limit, max_limit in self.position_limits:
@@ -106,6 +125,6 @@ class Optimizer(object):
                 max_limit
             )
 
-            for i, player in enumerate(self.players):
+            for i, player in self.enumerated_players:
                 if position == player.pos:
                     position_cap.SetCoefficient(self.variables[i], 1)
