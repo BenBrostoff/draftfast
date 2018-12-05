d5d236db753c9398e03c82f44817e815e10d5d3c
Ben Brostoff <ben.brostoff@gmail.com>
2018-12-05 16:28:50 -0500

    Add new FD NBA uploader

--- draftfast/csv_parse/uploaders.py
+++ draftfast/csv_parse/uploaders.py
@@ -1,8 +1,9 @@
 import csv
 from .nba_upload import (
-    map_pids as dk_nba_map_pids,
-    write_to_csv as dk_nba_write_to_csv,
+    map_pids,
+    write_to_csv,
 )
+from draftfast.rules import DRAFT_KINGS, FAN_DUEL
 
 
 class CSVUploader(object):
@@ -12,7 +13,7 @@ class CSVUploader(object):
         self.pid_map = self._map_pids(pid_file)
 
     def _map_pids(self, pid_file):
-        raise NotImplementedError('You must implement map_pids')
+        raise NotImplementedError('You must implement _map_pids')
 
 
 class DraftKingsNBAUploader(CSVUploader):
@@ -26,14 +27,14 @@ class DraftKingsNBAUploader(CSVUploader):
             writer = csv.writer(f)
             writer.writerow(self.HEADERS)
             for roster in rosters:
-                dk_nba_write_to_csv(
+                write_to_csv(
                     writer=writer,
                     roster=roster,
                     player_map=self.pid_map,
                 )
 
     def _map_pids(self, pid_file):
-        return dk_nba_map_pids(pid_file)
+        return map_pids(pid_file, game=DRAFT_KINGS)
 
 
 class DraftKingsNFLUploader(CSVUploader):
@@ -41,7 +42,25 @@ class DraftKingsNFLUploader(CSVUploader):
 
 
 class FanDuelNBAUploader(CSVUploader):
-    pass
+    HEADERS = [
+        'PG', 'PG', 'SG', 'SG', 'SF',
+        'SF', 'PF', 'PF', 'C',
+    ]
+
+    def write_rosters(self, rosters):
+        with open(self.upload_file, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(self.HEADERS)
+            for roster in rosters:
+                write_to_csv(
+                    writer=writer,
+                    roster=roster,
+                    player_map=self.pid_map,
+                    game=FAN_DUEL,
+                )
+
+    def _map_pids(self, pid_file):
+        return map_pids(pid_file, game=FAN_DUEL)
 
 
 class FanDuelNFLUploader(CSVUploader):
--- draftfast/test/test_csv_upload.py
+++ draftfast/test/test_csv_upload.py
@@ -45,3 +45,41 @@ def test_dk_nba_upload():
             '11743024',
         ]
     )
+
+
+def test_fd_nba_upload():
+    players = salary_download.generate_players_from_csvs(
+        game=rules.FAN_DUEL,
+        salary_file_location='{}/data/fd-nba-salaries.csv'.format(CURRENT_DIR),
+    )
+    roster = optimize.run(
+        rule_set=rules.FD_NBA_RULE_SET,
+        player_pool=players,
+        verbose=True,
+    )
+    upload_file = '{}/data/current-upload.csv'.format(CURRENT_DIR)
+    uploader = uploaders.FanDuelNBAUploader(
+        pid_file='{}/data/fd-nba-pids.csv'.format(CURRENT_DIR),
+        upload_file=upload_file,
+    )
+    uploader.write_rosters([roster])
+
+    row = None
+    with open(upload_file, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for idx, row in enumerate(reader):
+            if idx == 0:
+                continue
+    assert_equal(
+        row,
+        [
+            '11743190',
+            '11743146',
+            '11743013',
+            '11743142',
+            '11743007',
+            '11743176',
+            '11743369',
+            '11743024',
+        ]
+    )
