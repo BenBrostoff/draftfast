90847427f0c6dcb9c9cbab17838532f1460c414d
Ben Brostoff <ben.brostoff@gmail.com>
2021-08-01 13:02:57 -0400

    Bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
 
 setuptools.setup(
     name='draftfast',
-    version='3.5.1',
+    version='3.5.2',
     author='Ben Brostoff',
     author_email='ben.brostoff@gmail.com',
     description='A tool to automate and optimize DraftKings and FanDuel '
