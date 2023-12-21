2299bbc7a9593b8ecf7fdf1e2ed319d5b8e5248e
Nathan Pinciak <natepinciak@live.com>
2023-12-21 11:02:03 -0500

    Update `POSITIONS` to use new `POSITIONS_BY_SITE_BY_LEAGUE`

--- draftfast/rules.py
+++ draftfast/rules.py
@@ -1,6 +1,7 @@
+from draftfast.constants.positions import POSITIONS_BY_SITE_BY_LEAGUE
+
 DRAFT_KINGS = 'DRAFT_KINGS'
 FAN_DUEL = 'FAN_DUEL'
-
 
 ROSTER_SIZE = {
     DRAFT_KINGS: {
@@ -102,131 +103,6 @@ def get_nfl_showdown_positions(dk: bool = False, fd: bool = False) -> list:
     ]
 
 
-POSITIONS = {
-    DRAFT_KINGS: {
-        'NBA': [
-            ['PG', 1, 3],
-            ['SG', 1, 3],
-            ['SF', 1, 3],
-            ['PF', 1, 3],
-            ['C', 1, 2]
-        ],
-        'NBA_SHOWDOWN': [
-            ['CPT', 1, 1],
-            ['FLEX', 5, 5],
-        ],
-        'WNBA': [
-            ['PG', 1, 3],
-            ['SG', 1, 3],
-            ['SF', 1, 4],
-            ['PF', 1, 4],
-        ],
-        'NFL': get_nfl_positions(),
-        'NFL_SHOWDOWN': get_nfl_showdown_positions(dk=True),
-        'MLB': [
-            ['SP', 0, 2],
-            ['RP', 0, 2],
-            ['C', 1, 1],
-            ['1B', 1, 1],
-            ['2B', 1, 1],
-            ['3B', 1, 1],
-            ['SS', 1, 1],
-            ['OF', 3, 3],
-        ],
-        'SOCCER': [
-            ['F', 2, 3],
-            ['M', 2, 3],
-            ['D', 2, 3],
-            ['GK', 1, 2],
-        ],
-        'EL': [
-            ['G', 2, 3],
-            ['F', 3, 4],
-        ],
-        'NHL': [
-            ['C', 2, 3],
-            ['W', 3, 4],
-            ['D', 2, 3],
-            ['G', 1, 1],
-        ],
-        'NHL_SHOWDOWN': [
-            ['FLEX', 6, 6],
-        ],
-        'MLB_SHOWDOWN': [
-            ['CPT', 1, 1],
-            ['FLEX', 5, 5],
-        ],
-        'XFL': [
-            ['QB', 1, 1],
-            ['RB', 1, 3],
-            ['WR', 2, 4],
-            ['DST', 1, 1]
-        ],
-        'TEN': [
-            ['P', 6, 6],
-        ],
-        'PGA': [
-            ['G', 6, 6],
-        ],
-        'PGA_CAPTAIN': [
-            ['CPT', 1, 1],
-            ['G', 5, 5],
-        ],
-        'CSGO_SHOWDOWN': [
-            ['CPT', 1, 1],
-            ['FLEX', 5, 5],
-        ],
-        'F1_SHOWDOWN': [
-            ['CPT', 1, 1],
-            ['D', 4, 4],
-            ['CNSTR', 1, 1],
-        ],
-        'NASCAR': [
-            ['D', 6, 6],
-        ]
-    },
-    FAN_DUEL: {
-        'NBA': [
-            ['PG', 2, 2],
-            ['SG', 2, 2],
-            ['SF', 2, 2],
-            ['PF', 2, 2],
-            ['C', 1, 1],
-        ],
-        'MLB': [
-            ['P', 1, 1],
-            ['1B', 1, 2],  # TODO - allow C or 1B
-            ['2B', 1, 2],
-            ['3B', 1, 2],
-            ['SS', 1, 2],
-            ['OF', 3, 4],
-        ],
-        'MLB_MVP': [
-            ['MVP', 1, 1],
-            ['STAR', 1, 1],
-            ['UTIL', 3, 3],
-        ],
-        'NBA_MVP': [
-            ['MVP', 1, 1],
-            ['STAR', 1, 1],
-            ['PRO', 1, 1],
-            ['UTIL', 2, 2],
-        ],
-        'WNBA': [
-            ['G', 3, 3],
-            ['F', 4, 4],
-        ],
-        'NFL': get_nfl_positions(d_abbrev='D'),
-        'NFL_MVP': get_nfl_showdown_positions(fd=True),
-        'NASCAR': [
-            ['D', 5, 5],
-        ],
-        'PGA': [
-            ['G', 6, 6],
-        ],
-    }
-}
-
 NBA_GENERAL_POSITIONS = [
     ['G', 3, 4],
     ['F', 3, 4],
@@ -271,7 +147,7 @@ DK_NBA_RULE_SET = RuleSet(
     league='NBA',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['NBA'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['NBA'],
-    position_limits=POSITIONS[DRAFT_KINGS]['NBA'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['NBA'],
     general_position_limits=NBA_GENERAL_POSITIONS,
 )
 
@@ -280,7 +156,7 @@ DK_NBA_SHOWDOWN_RULE_SET = RuleSet(
     league='NBA_SHOWDOWN',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['NBA_SHOWDOWN'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['NBA_SHOWDOWN'],
-    position_limits=POSITIONS[DRAFT_KINGS]['NBA_SHOWDOWN'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['NBA_SHOWDOWN'],
     game_type='showdown',
     general_position_limits=[],
 )
@@ -290,7 +166,7 @@ FD_NBA_RULE_SET = RuleSet(
     league='NBA',
     roster_size=ROSTER_SIZE[FAN_DUEL]['NBA'],
     salary_max=SALARY_CAP[FAN_DUEL]['NBA'],
-    position_limits=POSITIONS[FAN_DUEL]['NBA'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[FAN_DUEL]['NBA'],
     general_position_limits=NBA_GENERAL_POSITIONS,
 )
 
@@ -299,7 +175,7 @@ DK_WNBA_RULE_SET = RuleSet(
     league='WNBA',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['WNBA'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['WNBA'],
-    position_limits=POSITIONS[DRAFT_KINGS]['WNBA'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['WNBA'],
     general_position_limits=NBA_GENERAL_POSITIONS,
 )
 
@@ -308,7 +184,7 @@ FD_WNBA_RULE_SET = RuleSet(
     league='WNBA',
     roster_size=ROSTER_SIZE[FAN_DUEL]['WNBA'],
     salary_max=SALARY_CAP[FAN_DUEL]['WNBA'],
-    position_limits=POSITIONS[FAN_DUEL]['WNBA'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[FAN_DUEL]['WNBA'],
     general_position_limits=NBA_GENERAL_POSITIONS,
 )
 
@@ -317,7 +193,7 @@ DK_NFL_RULE_SET = RuleSet(
     league='NFL',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['NFL'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['NFL'],
-    position_limits=POSITIONS[DRAFT_KINGS]['NFL'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['NFL'],
     offensive_positions=['QB', 'RB', 'WR', 'TE'],
     defensive_positions=['DST'],
     general_position_limits=[],
@@ -328,7 +204,7 @@ FD_NFL_RULE_SET = RuleSet(
     league='NFL',
     roster_size=ROSTER_SIZE[FAN_DUEL]['NFL'],
     salary_max=SALARY_CAP[FAN_DUEL]['NFL'],
-    position_limits=POSITIONS[FAN_DUEL]['NFL'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[FAN_DUEL]['NFL'],
     offensive_positions=['QB', 'RB', 'WR', 'TE'],
     defensive_positions=['D'],
     general_position_limits=[],
@@ -339,7 +215,7 @@ DK_NFL_SHOWDOWN_RULE_SET = RuleSet(
     league='NFL_SHOWDOWN',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['NFL_SHOWDOWN'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['NFL_SHOWDOWN'],
-    position_limits=POSITIONS[DRAFT_KINGS]['NFL_SHOWDOWN'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['NFL_SHOWDOWN'],
     offensive_positions=['CPT'],
     defensive_positions=['DST'],
     general_position_limits=[],
@@ -351,7 +227,7 @@ FD_NFL_MVP_RULE_SET = RuleSet(
     league='NFL_MVP',
     roster_size=ROSTER_SIZE[FAN_DUEL]['NFL_MVP'],
     salary_max=SALARY_CAP[FAN_DUEL]['NFL_MVP'],
-    position_limits=POSITIONS[FAN_DUEL]['NFL_MVP'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[FAN_DUEL]['NFL_MVP'],
     offensive_positions=['CPT'],
     defensive_positions=['D'],
     general_position_limits=[],
@@ -363,7 +239,7 @@ FD_MLB_MVP_RULE_SET = RuleSet(
     league='MLB_MVP',
     roster_size=ROSTER_SIZE[FAN_DUEL]['MLB_MVP'],
     salary_max=SALARY_CAP[FAN_DUEL]['MLB_MVP'],
-    position_limits=POSITIONS[FAN_DUEL]['MLB_MVP'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[FAN_DUEL]['MLB_MVP'],
     general_position_limits=[],
     game_type='showdown',
 )
@@ -373,7 +249,7 @@ FD_NBA_MVP_RULE_SET = RuleSet(
     league='NBA_MVP',
     roster_size=ROSTER_SIZE[FAN_DUEL]['NBA_MVP'],
     salary_max=SALARY_CAP[FAN_DUEL]['NBA_MVP'],
-    position_limits=POSITIONS[FAN_DUEL]['NBA_MVP'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[FAN_DUEL]['NBA_MVP'],
     general_position_limits=[],
     game_type='showdown',
 )
@@ -383,7 +259,7 @@ FD_PGA_RULE_SET = RuleSet(
     league='PGA',
     roster_size=ROSTER_SIZE[FAN_DUEL]['PGA'],
     salary_max=SALARY_CAP[FAN_DUEL]['PGA'],
-    position_limits=POSITIONS[FAN_DUEL]['PGA'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[FAN_DUEL]['PGA'],
     general_position_limits=[],
 )
 
@@ -392,7 +268,7 @@ DK_PGA_RULE_SET = RuleSet(
     league='PGA',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['PGA'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['PGA'],
-    position_limits=POSITIONS[DRAFT_KINGS]['PGA'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['PGA'],
     general_position_limits=[],
 )
 
@@ -401,7 +277,7 @@ DK_PGA_SHOWDOWN_CAPTAIN_RULE_SET = RuleSet(
     league='PGA_CAPTAIN',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['PGA_CAPTAIN'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['PGA_CAPTAIN'],
-    position_limits=POSITIONS[DRAFT_KINGS]['PGA_CAPTAIN'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['PGA_CAPTAIN'],
     general_position_limits=[],
     game_type='showdown',
 )
@@ -412,7 +288,7 @@ FD_NASCAR_RULE_SET = RuleSet(
     league='NASCAR',
     roster_size=ROSTER_SIZE[FAN_DUEL]['NASCAR'],
     salary_max=SALARY_CAP[FAN_DUEL]['NASCAR'],
-    position_limits=POSITIONS[FAN_DUEL]['NASCAR'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[FAN_DUEL]['NASCAR'],
     general_position_limits=[],
 )
 
@@ -422,7 +298,7 @@ DK_NASCAR_RULE_SET = RuleSet(
     league='NASCAR',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['NASCAR'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['NASCAR'],
-    position_limits=POSITIONS[DRAFT_KINGS]['NASCAR'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['NASCAR'],
     general_position_limits=[],
 )
 
@@ -433,7 +309,7 @@ DK_MLB_RULE_SET = RuleSet(
     max_players_per_team=7,
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['MLB'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['MLB'],
-    position_limits=POSITIONS[DRAFT_KINGS]['MLB'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['MLB'],
     general_position_limits=MLB_GENERAL_POSITIONS,
     min_teams=3,
     position_per_team_rules=[[lambda pos: 'P' not in pos, 5]],
@@ -444,7 +320,7 @@ FD_MLB_RULE_SET = RuleSet(
     league='MLB',
     roster_size=ROSTER_SIZE[FAN_DUEL]['MLB'],
     salary_max=SALARY_CAP[FAN_DUEL]['MLB'],
-    position_limits=POSITIONS[FAN_DUEL]['MLB'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[FAN_DUEL]['MLB'],
     general_position_limits=[],
 )
 
@@ -453,7 +329,7 @@ DK_SOCCER_RULE_SET = RuleSet(
     league='SOCCER',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['SOCCER'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['SOCCER'],
-    position_limits=POSITIONS[DRAFT_KINGS]['SOCCER'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['SOCCER'],
     offensive_positions=['M', 'F'],
     defensive_positions=['GK', 'D'],
     general_position_limits=[],
@@ -464,7 +340,7 @@ DK_EURO_LEAGUE_RULE_SET = RuleSet(
     league='EL',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['EL'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['EL'],
-    position_limits=POSITIONS[DRAFT_KINGS]['EL'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['EL'],
     general_position_limits=[],
 )
 
@@ -473,7 +349,7 @@ DK_NHL_RULE_SET = RuleSet(
     league='NHL',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['NHL'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['NHL'],
-    position_limits=POSITIONS[DRAFT_KINGS]['NHL'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['NHL'],
     offensive_positions=['C', 'W'],
     defensive_positions=['G', 'D'],
     general_position_limits=[],
@@ -484,7 +360,7 @@ DK_NHL_SHOWDOWN_RULE_SET = RuleSet(
     league='NHL_SHOWDOWN',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['NHL_SHOWDOWN'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['NHL_SHOWDOWN'],
-    position_limits=POSITIONS[DRAFT_KINGS]['NHL_SHOWDOWN'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['NHL_SHOWDOWN'],
     offensive_positions=['C', 'W'],
     defensive_positions=['G', 'D'],
     general_position_limits=[],
@@ -505,7 +381,7 @@ DK_MLB_SHOWDOWN_RULE_SET = RuleSet(
     league='MLB_SHOWDOWN',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['MLB_SHOWDOWN'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['MLB_SHOWDOWN'],
-    position_limits=POSITIONS[DRAFT_KINGS]['MLB_SHOWDOWN'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['MLB_SHOWDOWN'],
     general_position_limits=[],
     game_type='showdown'
 )
@@ -516,7 +392,7 @@ DK_XFL_CLASSIC_RULE_SET = RuleSet(
     league='XFL',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['XFL'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['XFL'],
-    position_limits=POSITIONS[DRAFT_KINGS]['XFL'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['XFL'],
     general_position_limits=[],
 )
 
@@ -526,7 +402,7 @@ DK_TEN_CLASSIC_RULE_SET = RuleSet(
     league='TEN',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['TEN'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['TEN'],
-    position_limits=POSITIONS[DRAFT_KINGS]['TEN'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['TEN'],
     general_position_limits=[],
 )
 
@@ -535,7 +411,7 @@ DK_CSGO_SHOWDOWN = RuleSet(
     league='CSGO_SHOWDOWN',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['CSGO_SHOWDOWN'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['CSGO_SHOWDOWN'],
-    position_limits=POSITIONS[DRAFT_KINGS]['CSGO_SHOWDOWN'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['CSGO_SHOWDOWN'],
     game_type='showdown',
     general_position_limits=[],
     max_players_per_team=3,
@@ -546,7 +422,7 @@ DK_F1_SHOWDOWN = RuleSet(
     league='F1_SHOWDOWN',
     roster_size=ROSTER_SIZE[DRAFT_KINGS]['F1_SHOWDOWN'],
     salary_max=SALARY_CAP[DRAFT_KINGS]['F1_SHOWDOWN'],
-    position_limits=POSITIONS[DRAFT_KINGS]['F1_SHOWDOWN'],
+    position_limits=POSITIONS_BY_SITE_BY_LEAGUE[DRAFT_KINGS]['F1_SHOWDOWN'],
     game_type='showdown',
     general_position_limits=[],
     max_players_per_team=2,
